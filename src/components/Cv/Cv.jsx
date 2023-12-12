bb9868845edddffa7e0e3555698f7436ae02f601
afsana-jannat <afsanajannat205@gmail.com>
2023-12-12 16:34:22 +0600

    all ok

--- src/components/Cv/Cv.jsx
+++ src/components/Cv/Cv.jsx
@@ -5,7 +5,7 @@ const Cv = () => {
         <div className="font-bold mt-6 mb-6 text-center text-black">
             <h2>LEARN MORE ABOUT ME</h2>
             <h2 className="mt-4 text-xl">SEE MY RESUME:
-                <a className="link link-primary"> https://drive.google.com/file/d/1BlMVtfzjLsEnviMF2RFpZm1dxZW2swzo/view?usp=sharing</a>
+                https://drive.google.com/file/d/1BlMVtfzjLsEnviMF2RFpZm1dxZW2swzo/view?usp=sharing
             </h2>
         </div>
     );
